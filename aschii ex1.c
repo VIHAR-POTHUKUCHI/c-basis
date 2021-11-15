63dd2b9c20dd77494a8c57db00f52204c5640c70
VIHAR-POTHUKUCHI <93147718+VIHAR-POTHUKUCHI@users.noreply.github.com>
2021-11-15 12:23:02 +0530

    Add files via upload

---
+++ aschii ex1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int n,m,o;
+	printf("enter the three numbers manually"); // the values are 4 2 21
+	scanf("%d%d%d",&n,&m,&o);
+		printf("%c%c%c",n+95,m+95,o+95);
+
+
+} 
