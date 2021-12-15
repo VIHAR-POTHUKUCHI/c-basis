966bea7aeccb57545708506ca469473a64bb8f42
VIHAR-POTHUKUCHI <93147718+VIHAR-POTHUKUCHI@users.noreply.github.com>
2021-12-15 11:50:48 +0530

    Add files via upload

---
+++ SWITCH CASE.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+void main()
+{
+	int month;
+	printf("ENTER the month");
+	scanf("%d",&month);
+	switch (month)
+	{
+		case 1:	
+		printf("THE CHOICE IS JANUARY");
+		break;
+		
+		case 2:
+		printf("THE CHOICE IS FEBURARY");
+		break;
+			case 3:
+		printf("THE CHOICE IS MARCH	");
+		break;
+			case 4:
+		printf("THE CHOICE IS  APRIL");
+		break;
+			case 5:
+		printf("THE CHOICE IS  MAY ");
+		break;
+			case 6:
+		printf("THE CHOICE IS JUNE");
+		break;
+			case 7:
+		printf("THE CHOICE IS JULY");
+		break;
+			case 8:
+		printf("THE CHOICE IS AUGUST");
+		break;
+			case 9:
+		printf("THE CHOICE IS SEPTEMBER");
+		break;
+			case 10:
+		printf("THE CHOICE IS OCTOBER");
+		break;
+			case 11:
+		printf("THE CHOICE IS NOVEMBER");
+break;
+	case 12:
+		printf("THE CHOICE IS DECEMBER");
+	break;	
+		
+		default:	
+		printf("invalid input");
+	
+		}
+	}
+	
+
